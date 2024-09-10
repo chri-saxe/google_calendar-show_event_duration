8ddcede60e896ff55b05fc6f1ebd30476668cc91
chri-saxe <154531461+chri-saxe@users.noreply.github.com>
2024-09-10 06:32:04 -0400

    2-3 : bugfix + presentation
    - Now works for events overlapping over two days, or ending at 00:00.

--- content.js
+++ content.js
@@ -11,8 +11,8 @@ function calcDuration(time) {
     
     if (end_time_min > start_time_min) { // Si dans la mm journée
         // Calc duration
-        end_time_min = (end_time_min == 0) ? 24 * 60 : end_time_min = end_time_min;
-        let duration_h = (end_time_min - start_time_min) / 60; // --> 19.25
+        // end_time_min = (end_time_min == 0) ? 1440 : end_time_min;
+        let duration_h = (end_time_min - start_time_min) / 60;
 
         // format
         if (String(duration_h).includes(".")) {
@@ -22,17 +22,26 @@ function calcDuration(time) {
         } else {
             duration_formatted = ` (${String(duration_h)}h)`;
         }
-    } else { // Si déborde sur la ou les journées suivantes
-        // A voir
+    } else { // Si déborde sur la journée suivante Eg : 21h30 21h15 = 1290 1275. 24h = 1440    => 1440 - 1290 + 1275
+        let duration_h = (1440 - start_time_min + end_time_min) / 60; // 1440min = 24h
 
+        // format
+        if (String(duration_h).includes(".")) {
+            duration_formatted = ((String(duration_h)[2] == ".") ? 
+                ` (${String(duration_h).slice(0, 2)}h${Number(String(duration_h).slice(2)) * 60})` :
+                ` (${String(duration_h).slice(0, 1)}h${Number(String(duration_h).slice(1)) * 60})`);
+        } else {
+            duration_formatted = ` (${String(duration_h)}h)`;
+        }
     }
 
     return duration_formatted;
 }
 
-function addEventDuration() {
+function addEventDuration(keydown) {
     document.querySelectorAll(".gVNoLb").forEach((i) => {
         let task_time_plus_duration = calcDuration(i.textContent);
+        if (keydown) {task_time_plus_duration = ""};
         i.textContent = i.textContent + task_time_plus_duration;
     })
 }
@@ -42,22 +51,25 @@ function removeEventDuration() {
         let task_time_minus_duration = i.textContent.slice(0, 13);
         i.textContent = task_time_minus_duration;
     })
-
 }
 
 
 
 console.log("(Extension) Connexion");
 
+keydown = false;
+
 document.addEventListener('keydown', (event) => {
     if (event.key === 'b' || event.key === 'B') {
-        addEventDuration();
+        addEventDuration(keydown);
+        keydown = true;
     }
 });
 
 document.addEventListener('keyup', (event) => {
     if (event.key === 'b' || event.key === 'B') {
         removeEventDuration();
+        keydown = false;
     }
 });
 
