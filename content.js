1301646d144947c44dfca8ea28c9ed3cd54ee0dd
chri-saxe <154531461+chri-saxe@users.noreply.github.com>
2024-09-10 17:08:18 -0400

    2-5 : refactoring

--- content.js
+++ content.js
@@ -1,38 +1,28 @@
-function calcDuration(time) {
+function calcDuration(task_time) {
     // format
-    let start_time = time.slice(0, 5);
-    let end_time = time.slice(8);
+    let start_time = task_time.slice(0, 5);
+    let end_time = task_time.slice(8);
     
     // convert to min
     let start_time_min = Number(start_time.slice(0, 2) * 60) + Number(start_time.slice(3));
     let end_time_min = Number(end_time.slice(0, 2) * 60) + Number(end_time.slice(3));
     
+    // calc duration
+    let duration_h = 0;
+    if (end_time_min > start_time_min) {
+        duration_h = (end_time_min - start_time_min) / 60;
+    } else { // Si event déborde sur la journée suivante
+        duration_h = (1440 - start_time_min + end_time_min) / 60; // 1440min = 24h
+    }
+
+    // format
     let duration_formatted = "";
-    
-    if (end_time_min > start_time_min) { // Si dans la mm journée
-        // Calc duration
-        // end_time_min = (end_time_min == 0) ? 1440 : end_time_min;
-        let duration_h = (end_time_min - start_time_min) / 60;
-
-        // format
-        if (String(duration_h).includes(".")) {
-            duration_formatted = ((String(duration_h)[2] == ".") ? 
-                ` (${String(duration_h).slice(0, 2)}h${Number(String(duration_h).slice(2)) * 60})` :
-                ` (${String(duration_h).slice(0, 1)}h${Number(String(duration_h).slice(1)) * 60})`);
-        } else {
-            duration_formatted = ` (${String(duration_h)}h)`;
-        }
-    } else { // Si déborde sur la journée suivante Eg : 21h30 21h15 = 1290 1275. 24h = 1440    => 1440 - 1290 + 1275
-        let duration_h = (1440 - start_time_min + end_time_min) / 60; // 1440min = 24h
-
-        // format
-        if (String(duration_h).includes(".")) {
-            duration_formatted = ((String(duration_h)[2] == ".") ? 
-                ` (${String(duration_h).slice(0, 2)}h${Number(String(duration_h).slice(2)) * 60})` :
-                ` (${String(duration_h).slice(0, 1)}h${Number(String(duration_h).slice(1)) * 60})`);
-        } else {
-            duration_formatted = ` (${String(duration_h)}h)`;
-        }
+    if (String(duration_h).includes(".")) {
+        duration_formatted = ((String(duration_h)[2] == ".") ? 
+            ` (${String(duration_h).slice(0, 2)}h${Number(String(duration_h).slice(2)) * 60})` :
+            ` (${String(duration_h).slice(0, 1)}h${Number(String(duration_h).slice(1)) * 60})`);
+    } else {
+        duration_formatted = ` (${String(duration_h)}h)`;
     }
 
     return duration_formatted;
@@ -72,28 +62,3 @@ document.addEventListener('keyup', (event) => {
         keydown = false;
     }
 });
-
-
-// Use MutationObserver to watch for changes in the DOM
-// let observer = new MutationObserver((mutations) => {
-//     updateContent();
-// });
-
-// // Configure the observer to watch for changes in the entire document body
-// observer.observe(document.body, {
-//     childList: true,
-//     subtree: true
-// });
-
-// // Optional: run initially to handle the existing content
-// updateContent();
-
-
-// window.onload = function() {
-//     setTimeout(() => {
-//         for (let i of document.querySelectorAll(".gVNoLb")) {
-//             let new_task_time = calcDuration(i.textContent);
-//             i.textContent = i.textContent + new_task_time;
-//         };
-//     }, 500);
-// }
